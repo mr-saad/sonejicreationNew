eb0a057488622e6340c6850baf75d0704374ea62
Saad Khatri <82932419+mr-saad@users.noreply.github.com>
2023-10-28 13:20:54 +0530

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config"
 import vercel from "@astrojs/vercel/serverless"
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: ["cdn.sanity.io", "as1.ftcdn.net"],
+    domains: ["cdn.sanity.io", "as1.ftcdn.net"]
   },
-  integrations: [],
+  integrations: [react()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 })
