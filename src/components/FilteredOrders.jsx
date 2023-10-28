60a9b3433ab6cc37b99b64ca9ec0b9a880ae4a35
Saad Khatri <82932419+mr-saad@users.noreply.github.com>
2023-10-28 13:13:08 +0530

    Add files via upload

---
+++ src/components/FilteredOrders.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react"
+import styles from "../../styles/FilteredOrders.module.css"
+
+export default function FilteredOrders({ allOrders }) {
+  const [filteredOrders, setFilteredOrders] = useState([])
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => {
+    setFilteredOrders(
+      allOrders.filter(all => all.userId === localStorage.getItem("userId"))
+    )
+    setMounted(true)
+  }, [])
+
+  return filteredOrders.length > 0
+    ? filteredOrders.map(({ productTitle, productImg, orderId }) => (
+        <a
+          href={"/orders/" + orderId}
+          key={orderId}
+          className={styles.card + " card radius"}
+        >
+          <img
+            src={productImg}
+            width={75}
+            className={styles.image}
+            height={75}
+          />
+
+          <div className={styles.details}>
+            <p>{productTitle}</p>
+          </div>
+        </a>
+      ))
+    : "You Haven't Ordered Anything Yet"
+}
--- src/pages/api/shop.js
+++ src/pages/api/shop.js
@@ -1,31 +1,31 @@
 import { google } from "googleapis"
+const creds = JSON.parse(
+  Buffer.from(import.meta.env.creds, "base64").toString()
+)
+const auth = new google.auth.GoogleAuth({
+  credentials: {
+    client_id: creds.client_id,
+    client_email: creds.client_email,
+    private_key: creds.private_key
+  },
+  scopes: "https://www.googleapis.com/auth/spreadsheets"
+})
+export const sheets = google.sheets({
+  version: "v4",
+  auth
+})
 
 export const POST = async ({ request }) => {
-  const body = await request.json()
-  const creds = JSON.parse(
-    Buffer.from(import.meta.env.creds, "base64").toString()
-  )
-
   try {
-    const auth = new google.auth.GoogleAuth({
-      credentials: {
-        client_id: creds.client_id,
-        client_email: creds.client_email,
-        private_key: creds.private_key,
-      },
-      scopes: "https://www.googleapis.com/auth/spreadsheets",
-    })
-    const sheets = google.sheets({
-      version: "v4",
-      auth,
-    })
+    const body = await request.json()
+
     await sheets.spreadsheets.values.append({
       spreadsheetId: import.meta.env.spreadsheetId,
-      range: "Orders!A2:E2",
+      range: "Orders!A2:G2",
       valueInputOption: "USER_ENTERED",
       resource: {
-        values: [body],
-      },
+        values: [body]
+      }
     })
     return new Response(
       JSON.stringify({ message: "Order Successfully Booked!" })
