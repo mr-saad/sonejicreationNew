030543ce0e110f394dfe0863282eb96d4cc01ece
Saad Khatri <82932419+mr-saad@users.noreply.github.com>
2023-11-27 19:42:46 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config"
-import vercel from "@astrojs/vercel/edge"
+import vercel from "@astrojs/vercel/serverless"
 import react from "@astrojs/react"
 
 // https://astro.build/config
@@ -9,5 +9,7 @@ export default defineConfig({
   },
   integrations: [react()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+      edgeMiddleware:true
+  })
 })
