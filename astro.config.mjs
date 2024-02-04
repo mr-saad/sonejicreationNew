3f4fc13cf8149f71b218ed9c7120e8f74863e2c7
Saad Khatri <82932419+mr-saad@users.noreply.github.com>
2024-02-04 12:49:52 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
 import { defineConfig } from "astro/config"
-// import vercel from "@astrojs/vercel/serverless"
+import vercel from "@astrojs/vercel/serverless"
 import react from "@astrojs/react"
-import node from "@astrojs/node"
-// https://astro.build/config
+
 export default defineConfig({
   image: {
     domains: ["cdn.sanity.io"]
   },
   integrations: [react()],
   output: "server",
-  adapter: 
-    //vercel()
-    node({
-    mode: "standalone"
-  }),
+  adapter: vercel(),
   prefetch: {
     defaultStrategy: "viewport",
     prefetchAll: true
